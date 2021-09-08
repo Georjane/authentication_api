891d2e967c3645bceaa02099a0d428c0ac69c5c2
Georjane <pzgeorjane@gmail.com>
2021-09-08 16:55:02 +0100

    Configure sessions fr production n development

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "devdomain.com"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "devdomain.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
