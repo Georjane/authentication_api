ce8e98993cd3e3cc98b9946dca48b0d8c00c49e2
Georjane <pzgeorjane@gmail.com>
2021-09-08 16:45:46 +0100

    Add current user concern

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :sessions, only: :create
   resources :registrations, only: :create
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
   root to: "static#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
