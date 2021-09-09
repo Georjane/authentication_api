710b673b79d917f170cc18a8f16d2297855bc53d
Georjane <pzgeorjane@gmail.com>
2021-09-09 17:31:40 +0100

    Add hotels table

---
+++ app/models/hotel.rb
@@ -0,0 +1,3 @@
+class Hotel < ApplicationRecord
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_08_140807) do
+ActiveRecord::Schema.define(version: 2021_09_09_162918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "plpgsql"
+
+  create_table "hotels", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "image_url"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_hotels_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -24,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_09_08_140807) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "hotels", "users"
 end
