e4bdb0dde623ca8f25c77d71784a66681c40c1a9
Georjane <pzgeorjane@gmail.com>
2021-09-09 18:59:29 +0100

    generate favourite model

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,6 +1,7 @@
 class Hotel < ApplicationRecord
   belongs_to :user
-
+  has_many :favourites
+  
   validates_presence_of :title, :description, :image_url
   validates_uniqueness_of :title, :description, :image_url
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :hotels
-  
+  has_many :favourites
+
   validates_presence_of :username, :email
   validates_uniqueness_of :username, :email
 end
