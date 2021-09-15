b7e0838d6dbdcdabb10f80523a5e7f12b690b19d
Georjane <pzgeorjane@gmail.com>
2021-09-15 20:11:28 +0100

    add config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.allowed_cors_origins = "https://keen-fermat-a31782.netlify.app"
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
