4a17edea977ace707d90d964c87bb3b7f3027400
Georjane <pzgeorjane@gmail.com>
2021-09-15 23:33:38 +0100

    Fix deployment errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions, only: :create
-  resources :hotels only: %i[index create show destroy]
-  resources :favorites only: %i[index create show destroy]
+  resources :hotels, only: %i[index create show destroy]
+  resources :favorites, only: %i[index create show destroy]
   resources :registrations, only: :create
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
