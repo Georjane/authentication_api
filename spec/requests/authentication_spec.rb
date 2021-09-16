e0b169619070cd5463bbf8ee12d4e3c6e87577cf
Georjane <pzgeorjane@gmail.com>
2021-09-16 11:03:47 +0100

    Fix deployment errors

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Authentications", type: :request do
+RSpec.describe 'Authentications', type: :request do
   describe 'POST /sessions' do
     let(:user) { FactoryBot.create(:user, username: 'user1', email: 'user1@gmail.com', password: 'password') }
     it 'authenticates the user' do
       post '/sessions', params: { user: { username: 'user1', email: 'user1@gmail.com', password: 'password' } }
       # expect(response).to have_http_status(:created)
       expect(json).to eq({
-        status: :created,
-        logged_in: true,
-        user: user
-      })
+                           status: :created,
+                           logged_in: true,
+                           user: user
+                         })
     end
     it 'returns error when username does not exist' do
       post '/sessions', params: { username: 'ac', password: 'password' }
