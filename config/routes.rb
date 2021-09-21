b3d2938dde5b5f9afa1b22f1cc83b6ef317fb4c6
Georjane <pzgeorjane@gmail.com>
2021-09-21 14:50:15 +0100

    Remove root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,4 @@ Rails.application.routes.draw do
   resources :registrations, only: :create
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
-  root to: "static#home"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
