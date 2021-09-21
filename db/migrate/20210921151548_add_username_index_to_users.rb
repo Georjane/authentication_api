323255ef5e11dd8498622da42e8c5be7a92e47c4
Georjane <pzgeorjane@gmail.com>
2021-09-21 16:17:15 +0100

    Add index migration for email and username to ensure db uniqueness

---
+++ db/migrate/20210921151548_add_username_index_to_users.rb
@@ -0,0 +1,5 @@
+class AddUsernameIndexToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_index :users, :username, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_175404) do
+ActiveRecord::Schema.define(version: 2021_09_21_151301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_09_09_175404) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "favorites", "hotels"
