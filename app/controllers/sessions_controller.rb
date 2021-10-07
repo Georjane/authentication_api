625287ad301ca3432da64633f5cb27a06898420c
Georjane <pzgeorjane@gmail.com>
2021-10-07 07:22:26 +0100

    Change sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,37 @@
 class SessionsController < ApplicationController
   def create
-    user = User
-      .find_by(username: params['user']['username'])
-      .try(:authenticate, params['user']['password'])
-
-    if user
-      session[:user_id] = user.id
-      user_info = {
-        id: user[:id],
-        user: user[:username],
-        email: user[:email]
-      }
-      render json: {
-        status: :created,
-        logged_in: true,
-        user: user_info
-      }
+    if user && user.authenticate(params.require(:password))
+      render json: UserRepresenter.new(user).as_json, status: :created
     else
-      render json: { status: 401 }
+      render json: { status: 401 } 
     end
   end
+
+  private
+  def user
+    @user ||= User.find_by(username: params.require(:username))
+  end
+  # def create
+  #   user = User
+  #     .find_by(username: params['user']['username'])
+  #     .try(:authenticate, params['user']['password'])
+
+  #   if user
+  #     session[:user_id] = user.id
+  #     user_info = {
+  #       id: user[:id],
+  #       user: user[:username],
+  #       email: user[:email]
+  #     }
+  #     render json: {
+  #       status: :created,
+  #       logged_in: true,
+  #       user: user_info
+  #     }
+  #   else
+  #     render json: { status: 401 }
+  #   end
+  # end
 
   def destroy
     reset_session
