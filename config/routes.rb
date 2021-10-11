c8d6bfe47ab557721a7e93e1ce0dd6d71dc9be2e
Georjane <pzgeorjane@gmail.com>
2021-10-11 06:39:16 +0100

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions, only: %i[create destroy]
   resources :hotels, only: %i[index create show destroy]
-  resources :favorites, only: %i[index create show destroy]
+  resources :favorites, only: %i[index create]
   resources :registrations, only: :create
   match '/logout', to: 'sessions#destroy', via: :delete
 end
